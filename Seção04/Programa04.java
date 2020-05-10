5cc38019454ac67925689d6a61492a095e5131de
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-10 18:23:10 -0300

    Estrutura de decisão

---
+++ Seção04/Programa04.java
@@ -0,0 +1,19 @@
+package br.com.exemplo.secao04;
+//Estrutura de decisão
+public class programa04 {
+
+	public static void main(String[] args) {
+		
+		int num = 4;
+		
+		if(num > 5) {
+			System.out.println("Sim o numero "+ num + " é maior que 5");	
+		}else if(num == 5){
+			System.out.println("Sim o numero "+ num +" é igual a 5");
+		}else {
+			System.out.println("Sim o numero "+ num +" é menor que 5");
+		}
+
+	}
+
+}
