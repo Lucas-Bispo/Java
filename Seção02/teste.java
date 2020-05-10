f80d6b72a8133433b25c49b4cbb989d1fc160450
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-10 17:53:32 -0300

    Teste

---
+++ Seção02/teste.java
@@ -0,0 +1,11 @@
+package br.com.exemplo.secao02;
+
+public class teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá mundo...");
+
+	}
+
+}
