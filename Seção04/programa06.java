0f375ef1107da31d26750a9f5023e0d84b2952e5
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-10 18:26:16 -0300

    Switch

---
+++ Seção04/programa06.java
@@ -0,0 +1,25 @@
+package br.com.exemplo.secao04;
+//Switch
+public class programa06 {
+
+	public static void main(String[] args) {
+		
+		int num = 6;
+		
+		switch(num) {
+		case 1:
+			System.out.println("Onumero é 1");
+			break;
+		case 2:
+			System.out.println("O numero é 2");
+			break;
+		case 5:
+			System.out.println("O numero é 5");
+			break;
+		default:
+			System.out.println("default O numero é "+ num);
+		}
+
+	}
+
+}
