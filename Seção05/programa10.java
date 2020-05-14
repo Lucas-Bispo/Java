40c93888eca7304d15eb7779a8eb276c6ebd2cae
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-13 22:17:40 -0300

    Foreach

---
+++ Seção05/programa10.java
@@ -0,0 +1,15 @@
+package br.com.exemplo.secao05;
+
+public class programa10 {
+
+	public static void main(String[] args) {
+	
+		String nome = "LUCAS";
+		//para cada um dos caracteres da strings, imprima o caracter
+		for (char letra : nome.toCharArray()) {
+			System.out.println(letra);
+		}
+
+	}
+
+}
