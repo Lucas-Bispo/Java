9a075c643daed61130525bd3f1f918686778ae84
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-13 22:15:58 -0300

    For

---
+++ Seção05/programa09.java
@@ -0,0 +1,28 @@
+package br.com.exemplo.secao05;
+import java.util.Scanner;
+
+public class programa09 {
+
+	public static void main (String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		int idade = 1;
+		String nome;
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		for(int i = 0; i < 5; i++ ) {
+				System.out.println("Informe seu nome:");
+				nome = teclado.nextLine();
+				
+				System.out.print("Informe sua idade:");
+				//idade = teclado.nextInt(); //bug
+				idade = Integer.parseInt(teclado.nextLine());
+				if(idade > 0) {
+				System.out.println(nome + " tem " + idade + " anos");
+			    }
+		}
+		teclado.close();
+	}
+}
