4b7850e4aa82a047064a2ae3a4ded571c7236280
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-14 22:44:49 -0300

    Tipos de dados

---
+++ Seção06/programa12.java
@@ -0,0 +1,22 @@
+package br.com.exemplo.secao06;
+
+public class programa12 {
+
+	public static void main(String[] args) {
+		
+		//por padrão os números reais em java são considerados double
+		float preco = 23.4f;
+		double preco2 = 23.4;
+		//diferenÃ§a entre eles velocidade de acesso.
+		//com letra maiÃºscula apenas para realizar conversÃ£o  
+		Float preco3 = 44.5f;
+		Double preco4 = 45.5;
+		
+		System.out.println("int ->NUM1 = " + preco);
+		System.out.println("int ->NUM2 = " + preco2);
+		System.out.println("int ->NUM3 = " + preco3);
+		System.out.println("int ->NUM4 = " + preco4);
+
+	}
+
+}
