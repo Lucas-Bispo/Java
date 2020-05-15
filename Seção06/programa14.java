6739b3cb7e33479ea98c1b0443c7504eb3ec8f8a
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-14 22:47:22 -0300

    Tipos de dados

---
+++ Seção06/programa14.java
@@ -0,0 +1,31 @@
+package br.com.exemplo.secao06;
+
+public class programa14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean verdadeiro = true;
+		boolean falso = false;
+		
+		//tipos não primitivos 
+		
+		Boolean v = true;
+		Boolean f = false;
+		
+		System.out.println("Verdadeiro " + verdadeiro);
+		System.out.println("falso" +falso);
+		
+		System.out.println("V" +v);
+		System.out.println("F" +f);
+		
+		//0 FALSO 1 VERDADEIRO por isso não usa size para Boolean.
+		
+		if(verdadeiro) {
+			//System.out.println("É verdadeiro");
+		}else {
+			//System.out.println("é falso");
+		}//não precisa condicionar pois o sistema Boolean já retorna verdadeiro ou falso.
+
+	}
+
+}
