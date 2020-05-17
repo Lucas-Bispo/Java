a2eb95716948dca780de0bb4c15566e8160ba436
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-17 20:59:11 -0300

    vetor

---
+++ Seção07/programa16.java
@@ -0,0 +1,30 @@
+package br.com.exemplo.secao07;
+//vetores parte 1
+public class programa16 {
+  //array (vetores uni-dimensionais)
+	public static void main(String[] args) {
+	 //float vetores[10] 0...9
+	 //int numeros[5] 0...4 (n - 1) 5 - 1 = 4
+	 //vetor[indice];
+	 /*
+	 * numero[0] = 1
+	 * numero[1] = 3
+	 * numero[2] = 5
+	 * numero[3] = 7
+	 * numero[4] = 9
+	 * */
+	 //Declarando um vetor.
+		int vetor[];
+		//Declarando e especificando o tamanho do vetor
+		int numeros[] = new int[5];
+		//Declarar e iniciazar 
+		int outros_numeros[] = {1,3,5,7,9};
+			
+		float valores[] = new float[5];
+		char caracteres[] = new char[20];
+		String nomes[] = new String[4];
+		String frutas[] = {"Manga","Uva", "Banana"};
+
+	}
+
+}
