af724923e9820165adcc0e89de934a514550cd85
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-17 21:00:51 -0300

    Matrizes

---
+++ Seção07/programa18.java
@@ -0,0 +1,30 @@
+package br.com.exemplo.secao07;
+
+public class programa18 {
+//Matrizes
+	/*
+	 multi-dimenssionais
+	 [linhas][colunas]
+	 numeros[0][0] = 1
+	 numeros[0][1] = 2
+	 numeros[1][0] = 3
+	 numeros[1][1] = 4
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declara��o
+		int outro_numeros[][];
+		//declara��o e defini��o de tamanho
+		int numeros[][] = new int [3][3];
+		//Declara��o, defenir o tamanho e inicializar
+				int mais_numeros[][] = {{1,2,3},{4,5,6},{7,8,9}};
+				//Declarar uma matriz informada somente as linhas
+				int matriz[][] = new int [2][2];
+				matriz[0] = new int[5];
+				matriz[1] = new int[3];
+				
+				int nova_matriz[][] = {{1,2},{4,5,6,65,8},{9,10,11}};
+
+	}
+
+}
