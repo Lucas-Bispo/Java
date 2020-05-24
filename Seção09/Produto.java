c8ac91c6572a85fbb553d1fb2b5b957fde92b611
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-24 08:40:46 -0300

    Classe produto

---
+++ Seção09/Produto.java
@@ -0,0 +1,27 @@
+package br.com.exemplo.secao09;
+//classes
+/*
+ * O nome das classes inicia com letras maiusculas 
+ * O nome não deve conter: Acentuação, caracteres especiais, espaço.
+ * Nas classes java, não eciste a implemantação da função main()
+ * Toda classe Java possui a seguinte forma:
+ * O nome da classe Java possui a seguinte forma
+ * Tudo que estiver dentro das chaves faz parte da classe
+ * public class nome{
+ * 
+ * }
+ * 
+ * Atribitutos 
+ * São as caracteristicas da classe/molde/modelo de dados:
+ * Podemos entender atributos como variáveis da classe;
+ * Uma outra forma de nomenclatura para os atributos são estados ;
+ * Atributos são nomeados em letras minúsculas para os atributos, sem espaços 
+ * sem caracteres especiais, sem acentuação.
+ * */
+
+public class Produto {
+	String nome;
+	float preco;
+	float desconto;
+
+}
