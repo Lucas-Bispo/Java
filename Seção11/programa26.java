f15b5aafe5df1f94fdb9230a83a7ae02f38bda64
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-28 22:40:23 -0300

    Add files via upload

---
+++ Seção11/programa26.java
@@ -0,0 +1,16 @@
+package br.com.exemplo.secao11;
+
+public class programa26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cliente cli = new Cliente(65,"afjlkjsjf", "fldkjfskjfd , 256");
+		
+		System.out.println(cli);
+		//System.out.println("Nome "+ cli.nome);
+		//System.out.println("endereco:"+ cli.endereco);
+
+
+	}
+
+}
