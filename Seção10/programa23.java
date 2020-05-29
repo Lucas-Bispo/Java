3aac9970a719a35e5557497a9bf84cafa208575f
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-28 22:35:28 -0300

    Pacotes

---
+++ Seção10/programa23.java
@@ -0,0 +1,19 @@
+package br.com.exemplo.secao10;
+// Pacotes servem para organizar o código fonte do projeto
+// pacote indicam onde determinado arquivo do projeto se encontra
+//Os nomes de pacotes devem ser em letras minúscula, sem espaço,
+//sem caracteres especial.
+//Por convenção, é ultilizado nos nomes de pacotes, o dominio da empresa
+//criadora do projeto, ao contário, sendo ultima parte do nome 
+//o especificador do pacote;
+//Padrão de projeto
+//MVC model-View Controller
+public class programa23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
