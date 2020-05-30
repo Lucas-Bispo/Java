f569c82fe46037f335185c272ef30ae98ddd187b
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-29 22:21:20 -0300

    Sobrescrita de métodos

---
+++ Seção12/programa30.java
@@ -0,0 +1,15 @@
+package br.com.exemplo.secao12;
+//Sobrescrita de métodos
+/*
+ Overriding
+ Ocorre quando sobrescrevemos métodos da classe pai (super classe) em classes filhas.
+ 
+ */
+public class programa30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
