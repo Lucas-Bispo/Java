a5a2ae5bc230b20c9d72593ae657025b950abebd
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-05-29 22:20:45 -0300

    Sobrecarga de Método

---
+++ Seção12/programa29.java
@@ -0,0 +1,19 @@
+package br.com.exemplo.secao12;
+//Sobrecarga de Método
+/*
+ Overloading
+ Ocorre quando, sobrescrevemos um método na mesma classe de declaração.
+ */
+public class programa29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Pessoa p1 = new Pessoa();
+		//p1.menssagem();
+		//p1.menssagem("MENSSAGEM 2");
+		//p1.menssagem("MENSSAGEM 2 = ", 7);
+		
+
+	}
+
+}
