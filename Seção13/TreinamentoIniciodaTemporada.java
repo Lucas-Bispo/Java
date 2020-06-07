2321167a300db7417acc44a8cba2d8f41328a6ab
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-06-06 21:03:48 -0300

    Classe

---
+++ Seção13/TreinamentoIniciodaTemporada.java
@@ -0,0 +1,18 @@
+package br.com.exemplo.secao13;
+
+public class TreinamentoIniciodaTemporada extends Treinamento{
+
+	@Override
+	public void preparoFisico() {
+		// TODO Auto-generated method stub
+		System.out.println("Preparo físico de início de temporada");
+		
+	}
+
+	@Override
+	public void jogoTreino() {
+		// TODO Auto-generated method stub
+		System.out.println("Jogo treino de início de temporada...");
+	}
+
+}
